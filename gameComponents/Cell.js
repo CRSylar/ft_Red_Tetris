b2d5a02599e6435adad9cf336eeb7b51f2036f66
CRSylar <cristianoromaldetti@icloud.com>
2022-02-22 18:54:46 +0100

    Creato campo da gioco e Aggiunto Styling ai componenti base, da finire lo styling dei componenti laterali

--- gameComponents/Cell.js
+++ gameComponents/Cell.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Box from "@mui/material/Box";
+import styled from "styled-components";
+import {TETROMINOES} from "./tetrominoes";
 
-function Cell (props) {
+const StyledCell = styled.div`
+	width: auto;
+	background: rgba(${props => props.color}, 0.8);
+	border: ${props => (props.type === 0? '0px solid': '4px solid')};
+  border-bottom-color: rgba(${props => props.color}, 0.1);
+  border-right-color: rgba(${props => props.color}, 1);
+  border-top-color: rgba(${props => props.color}, 1);
+  border-left-color: rgba(${props => props.color}, 0.3);
+`
+
+
+function Cell ({type}) {
 	return (
-		<div>Cell</div>
+		<StyledCell type={'L'} color={TETROMINOES['L'].color} />
 	);
 }
 
--- gameComponents/Tetris.js
+++ gameComponents/Tetris.js
@@ -3,10 +3,34 @@ import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 import {createStage} from "../gameHelpers/Utility";
+import styled from "styled-components";
+
+const StyledTetrisWrapper = styled.div`
+	width: 100vw;
+	height: 100vh;
+`
+
+
+const StledTetris = styled.div`
+	display: flex;
+	align-items: flex-start;
+	padding: 40px;
+	margin: 0 auto;
+	max-width: 900px;
+	
+	aside {
+		width: 100%;
+		max-width: 200px;
+		display: block;
+		padding: 0 20px;
+	}
+`
 
 function Tetris (props) {
 	return (
-		<div>
+		<StyledTetrisWrapper>
+			<StledTetris>
+
 			<Stage stage={createStage()}/>
 			{/* <Aside > */}
 				<div>
@@ -17,7 +41,8 @@ function Tetris (props) {
 				<StartButton />
 			{/* </Aside> */}
 
-		</div>
+			</StledTetris>
+		</StyledTetrisWrapper>
 	);
 }
 
