acdf61cd74093f5f018f9fa3509af4e611accb42
CRSylar <cristianoromaldetti@icloud.com>
2022-03-29 10:44:20 +0200

    Now is working ! va fatto prisma init -> migration -> generate DENTRO il container cosi da avere istanza di prisma compilata nel modo giusto, Branch pronto

--- Nextjs/install.sh
+++ Nextjs/install.sh
@@ -4,6 +4,9 @@ yarn install
 if [[ $? == '0' ]]
 	then
 		echo "install ok start NextjsApp in DevMode"
+		yarn add prisma
+		npx prisma migrate dev --name containerMigration
+		npx prisma generate
 		yarn dev
 	else
 		echo 'Something Bad ! install failed'
